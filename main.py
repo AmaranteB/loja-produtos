956592c817ec3728cac6ca4966f768c716fdc9a1
AmaranteB <bernardo_azevedo_2@hotmail.com>
2025-11-24 17:15:47 -0300

    feat: versão inicial do projeto loja-produtos

---
+++ main.py
@@ -0,0 +1,18 @@
+from produto import Produto
+
+def main():
+    produto1 = Produto("Teclado", 350, estoque=10)
+    produto2 = Produto("Mouse", 150, estoque=20)
+
+    print("Lista dos produtos")
+    print(produto1)
+    print(produto2)
+
+    print("\n Vendendo 5 teclados...")
+    if produto1.vender(2):
+        print("Venda realizada com sucesso!")
+    else:
+        print("Estoque insuficiente.")
+
+if __name__ == "__main__":
+    main()
